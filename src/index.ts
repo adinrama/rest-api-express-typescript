5b94335fb5cdac6c4d47479d72bebccba16cf989
adinrama <adinrama@student.ub.ac.id>
2024-03-17 21:37:40 +0700

    Initialize project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+const port: Number = 5000;
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: "Hello, I'm Nainnunissa" });
+});
+
+app.listen(port, () =>
+  console.log(`Server is listening on http://localhost:${port}`)
+);
